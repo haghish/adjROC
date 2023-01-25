6a6f2bb19fc3f5832391e0fd244561265104f44c
haghish <haghish21@gmail.com>
2023-01-25 11:09:53 +0100

    remove temps

--- R/bootpval.R
+++
@@ -1,9 +0,0 @@
-
-
-bootpval <- function(x) {
-  require(boot.pval)
-  D <- mean(x$t, na.rm = TRUE) / sd(x$t, na.rm = TRUE)
-  p <- boot.pval::boot.pval(x, type = "norm", theta_null = 0)
-  result <- list(D = D, p = p)
-  return(result)
-}
--- R/test.auprc.R
+++
@@ -1,9 +0,0 @@
-
-
-test.auprc <- function(data, i) {
-  require(yardstick)
-  a <- yardstick::pr_auc_vec(data$class1[i],
-                             data$score1[i])
-  b <- yardstick::pr_auc_vec(data$class2[i], data$score2[i])
-  return(a - b)
-}
